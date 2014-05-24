49ca0bd23c0a36c0ae61de56323c7b77f89c571e
karpathy <andrej.karpathy@gmail.com>
2014-05-24 15:20:55 -0700

    adding ability to click any event and display more information about the event. In particular, interface provides the unix time for beginning of that event which can be used to log a note at the beginning of that event, in retrospect some time later. Also had to modify the note.sh script accordingly.

--- note.sh
+++ note.sh
@@ -6,10 +6,16 @@ mkdir -p notes
 
 read -p "Enter note: " n
 
-echo "$(date) new note saved:"
-echo $n
-
-T="$(date +%s)"
-echo "$T $n" >> notes/notes.txt
-
+if [ -z $1 ] 
+then 
+  # $1 was provided, use it
+  echo "saving new note \"$n\" for current time ($(date))"
+  T="$(date +%s)"
+  echo "$T $n" >> notes/notes.txt
+ 
+else
+  # no $1 provided, use current time
+  echo "saving new note \"$n\" for given time ($1)"
+  echo "$1 $n" >> notes/notes.txt
+fi
 
