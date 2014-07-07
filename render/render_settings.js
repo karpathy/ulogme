8824c908cdc04b17acb2a74d23c602f45df70eec
karpathy <andrej.karpathy@gmail.com>
2014-07-07 16:43:33 -0700

    fixing silly logic

--- render/render_settings.js
+++ render/render_settings.js
@@ -9,19 +9,21 @@ so it is recommended that only few possible outputs exist,
 and use of MISC category as shown in example is encouraged.
 */
 function mapwin(w) {
-  wbefore = w;
-  if(w.indexOf("Gmail")>-1) w = "Gmail";
-  if(w.indexOf("Inotebook")>-1) w = "INotebook";
-  if(w.indexOf("Google Chrome")>-1) w = "Google Chrome";
-  if(w.indexOf("karpathy@")>-1) w = "Terminal";
-  if(w.indexOf("MATLAB")>-1) w = "Matlab";
-  if(w.indexOf("Figure")>-1) w = "Matlab";
-  if(w.indexOf("Sublime Text")>-1) w = "Sublime Text 2";
-  if(w.indexOf("TeXworks")>-1) w = "Latex";
-  if(w === '__LOCKEDSCREEN') w = "Locked Screen";
+
+  bool match = false;
+  if(w.indexOf("Gmail")>-1) { w = "Gmail"; match = true; }
+  if(w.indexOf("Inotebook")>-1) { w = "INotebook"; match = true; }
+  if(w.indexOf("Google Chrome")>-1) { w = "Google Chrome"; match = true; }
+  if(w.indexOf("Firefox")>-1) { w = "Google Chrome"; match = true; }
+  if(w.indexOf("karpathy@")>-1) { w = "Terminal"; match = true; }
+  if(w.indexOf("MATLAB")>-1) { w = "Matlab"; match = true; }
+  if(w.indexOf("Figure")>-1) { w = "Matlab"; match = true; }
+  if(w.indexOf("Sublime Text")>-1) { w = "Sublime Text 2"; match = true; }
+  if(w.indexOf("TeXworks")>-1) { w = "Latex"; match = true; }
+  if(w === '__LOCKEDSCREEN') { w = "Locked Screen"; match = true; }
   // none of the rules matched? fold into MISC
-  if(w === wbefore) {
-    console.log('folding into MISC: ' + w);
+  if(!match) {
+    //console.log('folding into MISC: ' + w);
     w = 'MISC'; // none of the rules applied
   }
   return w;
